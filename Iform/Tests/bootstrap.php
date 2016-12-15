284c103f7c56b02c9c84d35288addd061da4f939
Seth Salinas <ssalinas@zerionsoftware.com>
2016-12-15 11:11:43 -0500

    fixed autoloader inc

--- Iform/Tests/bootstrap.php
+++ Iform/Tests/bootstrap.php
@@ -6,6 +6,6 @@ define('PAGE', '797455');
 define('SERVER', 'https://ssalinasdemo.iformbuilder.com/');
 define('PROFILE', '161521');
 
-$autoloader = require dirname(__DIR__) . '/../vendor/autoload.php';
+$autoloader = require dirname(__FILE__) . '/../vendor/autoload.php';
 
 
